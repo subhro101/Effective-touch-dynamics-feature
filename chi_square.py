ecb3ca8c5c18b2c575cc25d5c1124907d81a9168
ryanarj <karshan813@gmail.com>
2018-11-06 10:11:32 -0500

    Works,needs to tested on dataset

--- chi_square.py
+++ chi_square.py
@@ -1,6 +1,17 @@
  # File for the chi square feature selection algorithm
+import pandas
+from scipy.stats import chi2_contingency
 
+class ChiSquare():
 
-def get_features():
-    # TODO
+    def chi_square_dataframe_cols(self, dataframe, cols):
+        groupsizes = dataframe.groupby(cols).size()
+        ctsum = groupsizes.unstack(cols[0])
+        return(chi2_contingency(ctsum.fillna(0)))
 
+df = pandas.DataFrame([[0, 1, 4], [1, 0, 5], [0, 2, 6], [0, 1, 6], [0, 2, 8]], columns=['A', 'B', 'C'])
+stat, p, dof, expected = ChiSquare().chi_square_dataframe_cols(dataframe=df, cols=['A', 'B', 'C'])
+print('dof=%d' % dof)
+print(expected)
+print(stat)
+print(p)
